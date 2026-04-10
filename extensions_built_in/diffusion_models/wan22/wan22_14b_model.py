c04338c4521ffb91558e1ea251025aaa1fa2b6d3
jayn7 <fajarnauval70@gmail.com>
2026-04-10 19:49:33 +0700

    fix

--- extensions_built_in/diffusion_models/wan22/wan22_14b_model.py
+++ extensions_built_in/diffusion_models/wan22/wan22_14b_model.py
@@ -498,14 +498,16 @@ class Wan2214bModel(Wan21):
                 except (AttributeError, IndexError, TypeError):
                     continue
                 
-                delta_dev = delta.to(module.weight.device, dtype=module.weight.dtype)
                 if is_bias:
-                    if module.bias is not None:
+                    if hasattr(module, 'bias') and module.bias is not None:
+                        delta_dev = delta.to(module.bias.device, dtype=module.bias.dtype)
                         module.bias.data.add_(delta_dev)
                         applied += 1
                 else:
-                    module.weight.data.add_(delta_dev)
-                    applied += 1
+                    if hasattr(module, 'weight') and module.weight is not None:
+                        delta_dev = delta.to(module.weight.device, dtype=module.weight.dtype)
+                        module.weight.data.add_(delta_dev)
+                        applied += 1
         
         self.print_and_status_update(f"Applied {applied} diff weights to dual transformer")
 
@@ -534,14 +536,16 @@ class Wan2214bModel(Wan21):
                 except (AttributeError, IndexError, TypeError):
                     continue
                 
-                delta_dev = delta.to(module.weight.device, dtype=module.weight.dtype)
                 if is_bias:
-                    if module.bias is not None:
+                    if hasattr(module, 'bias') and module.bias is not None:
+                        delta_dev = delta.to(module.bias.device, dtype=module.bias.dtype)
                         module.bias.data.sub_(delta_dev)
                         removed += 1
                 else:
-                    module.weight.data.sub_(delta_dev)
-                    removed += 1
+                    if hasattr(module, 'weight') and module.weight is not None:
+                        delta_dev = delta.to(module.weight.device, dtype=module.weight.dtype)
+                        module.weight.data.sub_(delta_dev)
+                        removed += 1
         
         self.print_and_status_update(f"Removed {removed} diff weights from dual transformer")
 
